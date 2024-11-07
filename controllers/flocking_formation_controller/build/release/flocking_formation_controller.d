9d96df27dd8dac770208a1cae50939b0d5a11136
antoinehinary <antoine.hinary@epfl.ch>
2024-11-07 13:43:35 +0100

    antoine: added first version files

---
+++ controllers/flocking_formation_controller/build/release/flocking_formation_controller.d
@@ -0,0 +1,9 @@
+build/release/flocking_formation_controller.o: \
+ flocking_formation_controller.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/emitter.h \
+ /usr/local/webots/include/controller/c/webots/receiver.h
